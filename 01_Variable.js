9cc122ce251b21594c3ce22e0a6c7d604705a36a
Apgupta1234 <apgupta6393@gmail.com>
2024-07-23 12:13:42 +0530

    update 3

--- 01_Variable.js
+++ 01_Variable.js
@@ -2,7 +2,13 @@ const accountID = 31716
 let accountEmail = "apgupta1234@gmail.com"
 var accountPassword = "1234"
 accountCity = "newyork"
+accountState//It show the   Refrence error: accountState is  not deifned
 
-//accountID = 2
-accountEmail
-console.log(accountID);           
+accountState=""//It will run but ntg will be printed
+
+accountState="Uttar Pradesh"// pirnt the output Uttar Pradesh BUt here we use to prefer 'let' keyword
+
+//accountID = 2 Type Error
+
+      
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
